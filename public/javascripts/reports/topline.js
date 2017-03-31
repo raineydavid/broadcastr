e4c900fefa2f004747ba90e7980ef1b053c42944
AJ Kahle <akahle@270strategies.com>
2017-03-31 13:09:52 -0500

    link sourcing

---
+++ public/javascripts/reports/topline.js
@@ -0,0 +1,164 @@
+var host = location.origin.replace(/^http/, 'ws'),
+    ws = new WebSocket(host)
+
+var arrayToObjects = function(input,headers){
+  var array = new Array
+  input.forEach(function(row){
+    var output = new Object
+
+    row.forEach(function(col,i){
+      output[headers[i]] = col
+    })
+
+    array.push(output)
+  })
+  return array
+}
+
+//Page Setup
+
+var thisPage = '/reports/toplines',
+    pageHeight = window.innerHeight,
+    state = new Object,
+    cols = [
+    ],
+    headerOptions =[
+    ]
+
+//Date Format Function
+
+var dateFormat = function(date){
+  var splitDate = date.split("/")
+  return "20"+splitDate[2]+"-"+splitDate[0]+"-"+splitDate[1]
+}
+
+//MODALS
+
+var modalContent = d3.select("body").append("div").append("div")
+    .attr("class","modal fade")
+    .attr("id","newUser")
+    .attr("tabindex","-1")
+    .attr("role","dialog")
+    .attr("aria-labelledby","modal")
+    .append("div")
+      .attr("class","modal-dialog modal-sm")
+      .attr("role","document")
+      .style("width","30%")
+    .append("div")
+      .attr("class","modal-content")
+
+var modalBody = modalContent.append("div")
+  .attr("class","modal-body clearfix")
+  .style("padding-top","0px")
+  .style("text-align","center")
+
+modalBody.append("h2")
+  .attr("class","modal-title logoLightBlue-500")
+  .attr("id","modalHeader")
+  .style("margin-bottom","5px")
+  .text("Welcome to Echo!")
+
+modalBody.append("div")
+  .attr("class","row col-xs-12")
+  .style("margin-bottom","20px")
+  .text("Echo is a tool to help you have one-on-one conversations with grassroots supporters over email.")
+
+modalBody.append("div")
+  .attr("class","row col-xs-12")
+  .style("margin-bottom","20px")
+  .text("We connect to your email account to send emails as you, and supporters can reply directly to your inbox to start a conversation.")
+
+modalBody.append("div")
+  .attr("class","row col-xs-12")
+  .style("margin-bottom","20px")
+  .text("Click the button below to connect your email account and get started!")
+
+modalBody.append("div")
+  .attr("class","row")
+  .append("div")
+    .attr("class","buttonDiv col-xs-12")
+      .append("button")
+        .attr("class","btn btn-default logoLightBlue-bg-500")
+        .text("Connect My Email")
+        .style("margin","auto")
+        .on("click",function(){
+          window.location.replace('/gm/auth/url')
+        })
+
+//Tooltip
+
+var tooltipDisplay = function tooltipDisplay(msg){
+  tooltip.style('opacity', .9)
+    .style("display","block")
+  tooltip.html(msg)
+
+  if((d3.select("body").node().getBoundingClientRect().width-d3.event.clientX)<tooltip.node().getBoundingClientRect().width+50){
+    var tooltipX = -(20 + tooltip.node().getBoundingClientRect().width)
+  }else{var tooltipX = 20}
+
+  if((d3.select("body").node().getBoundingClientRect().height-d3.event.clientY)<tooltip.node().getBoundingClientRect().height+30){
+    var tooltipY = -(tooltip.node().getBoundingClientRect().height-20)
+  }else{var tooltipY = -15}
+
+  tooltip
+      .style('left', (d3.event.clientX + tooltipX) + 'px')
+      .style('top',  (d3.event.clientY + tooltipY) + 'px')
+}
+
+var tooltipHide = function tooltipHide(){
+  tooltip.style("display","none")
+}
+
+var tooltip = d3.select('body').append('div')
+        .attr('id','tooltip')
+        .style('position', 'absolute')
+        .style('padding', '0 10px')
+        .style('background', 'white')
+        .style("display","none")
+        .style("z-index","1051")
+        .style("font-family","'Gotham SSm A','Gotham SSm B','Helvetica Neue',Helvetica,Arial,sans-serif")
+        .style("font-size",'11px')
+
+ws.onopen = function(event){
+    console.log('connected')
+  }
+
+ws.onmessage = function(event){
+  var dataId = JSON.parse(event.data).id
+  var data = JSON.parse(event.data).data
+
+  switch(dataId){
+    case "pages":
+    console.log(data)
+      var body = d3.select("body")
+        navBar(body,data,function(d){
+          ws.send(JSON.stringify({type:"reports",id:"topline"}))
+        })
+
+    break;
+    case "data":
+      console.log(data)
+      var footer = d3.select("body").select("footer")
+
+      var contentDiv = d3.select("body").append("div")
+        .attr("class","container-fluid")
+
+      var columns = contentDiv.append("div")
+        .attr("class","row")
+        .selectAll("div")
+          .data(cols)
+          .enter().append("div")
+            .attr("class",function(d){
+              return d.class
+            })
+
+      columns.append("div")
+        .attr("class","row col-xs-12")
+        .append("h4")
+          .text(function(d){
+            return d.label
+          })
+
+    break;
+  }
+}
