74456a6be7cbe3198ee7afbc8ed3ac82f2a7615c
AJ Kahle <akahle@270strategies.com>
2017-05-15 18:03:39 -0500

    page test

--- routes/email/email.js
+++ routes/email/email.js
@@ -13,9 +13,8 @@ var include       = require('../../include').include,
 exports.getEmail = function(req,res){
   switch(req.params.page){
     case "send":
-      console.log(req.session.user)
-      if(req.session.user.tokens[0].type===null){
-        res.redirect('/email/send/#newUser');
+      if(req.session.user.tokens[0].type===null&&req.query.state!="newUser"){
+        res.redirect('/email/send?state=newUser#newUser');
       }else{
         res.send(templates.template.expand({
           title:"Echo - Send Email",
