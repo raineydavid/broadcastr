2ff1e4609e5660c70ae9e57d651443cf52eeea58
AJ Kahle <akahle@270strategies.com>
2017-06-19 14:28:54 -0500

    send fix

--- worker.js
+++ worker.js
@@ -8,12 +8,12 @@ app.queue.process('email',function(job,done){
   console.log("INFO - "+job.data.user.email + " Send Started")
   emailSend.send(job.data.user,job.data.client,job.data.recip,job.data.mergeFields,job.data.subj,job.data.body,function(tokenError,newTokens){
     if(tokenError){console.log(tokenError)}
-    client.query("UPDATE "+client.schema+".creds SET access_token = $1,refresh_token = $2 WHERE user_id = $3,type='gmail'",[newTokens.access_token,newTokens.refresh_token,job.data.user.user_id],function(err){
+    client.query("UPDATE "+client.schema+".creds SET access_token = $1,refresh_token = $2 WHERE user_id = $3 AND type='gmail'",[newTokens.access_token,newTokens.refresh_token,job.data.user.user_id],function(err){
       if(err){
         console.log(err)
         console.log("ERROR - Update Tokens")
       }
-      done()
+      done();
       console.log("INFO - "+job.data.user.email + " Send Done")
     })
   })
